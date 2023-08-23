0b233406a2c243d4655dad635ae5fa044d06c84e
ArulselvamAB <arulab2002@gmail.com>
2023-08-23 22:29:40 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>hello world</h1>',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'home';
+}
